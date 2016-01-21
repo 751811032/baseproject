42319a9d30638bdcf6bb56697608766ccb18a0ca
751811032 <751811032@qq.com>
2016-01-21 15:50:44 +0800

    Create Project's Main Java File.

---
+++ src/java/com/ct855/controller/page/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ct855.controller.page;
+
+/**
+ *
+ * @author rain
+ */
+public class NewClass {
+    
+}
